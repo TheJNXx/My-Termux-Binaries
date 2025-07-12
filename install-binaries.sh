e27f593edfc7b00cd4150e5a0734b7a14b3fc9c1
USERMAN7 <tokdlybrawla@gmail.com>
2025-07-12 23:30:33 +0500

    Comments

--- install-binaries.sh
+++ install-binaries.sh
@@ -9,51 +9,49 @@
 
 echo "This is a setting up utility for"
 echo "Installation of programms in this repo"
-# Want to try something better here. Like
-# putting ls into var then go like line by line
-# to install binary to $PATH or not.
-# Right now this is the hard way
 
-echo "Would you like to install chdman,flips,
-librespeed-cli,ytcast?"
-echo "Y/n ?"
-read -r install
-if [ $install == "Y" ]; then
+# Achieved of picky install :)
+
+echo "Would you like to install chdman,flips,librespeed-cli,ytcast?"
+echo "Y/n ?" # I actually removed \n because in most cases it would be better to eye,
+		# And with small screen res it would just resize automatically
+read -r install # input -r flag for avoiding weird symbols from backspace
+if [ $install == "Y" ]; then # Putting ALL binaries to termux default bin
 	chmod +x *
 	for i in "${!fields[@]}"; do
 		mv ${fields[$i]} /data/data/com.termux/files/usr/bin
 	done
-elif [ $install == "y" ]; then
+elif [ $install == "y" ]; then # Same thing 
 	chmod +x *
 	for i in "${!fields[@]}"; do
 		mv ${fields[$i]} /data/data/com.termux/files/usr/bin
 	done
-elif [ $install == "N" ]; then
+elif [ $install == "N" ]; then # BRAND NEW PICKY INSTALL!
 	echo "Whould you like to select which binaries you want to install?"
 	echo "Y/n ?"
-	read -r install2
+	read -r install2 # Man that's a mangle of elif,ifs.. Should change to cases 
 	if [ $install2 == "Y" ]; then
 	       for i in "${!fields[@]}"; do
 			echo "Would you like to install ${fields[$i]}? (Y/n)"
 			read -r install3
-			case "$install3" in
+			case "$install3" in # added cases
 			 [Yy])
 				 mv "${fields[$i]}" /data/data/com.termux/files/usr/bin || {
 					 echo "Failed to move binary: ${fields[$i]}"
-					 exit 1
+					 exit 1 # || that code under || triggers only if something fails
 				 }
 				 ;;
 			 [Nn])
-				 echo "Skipping ${fields[$i]}"
+				 echo "Skipping ${fields[$i]}" # picky install!
 				 ;;
 			      *)
-				 echo "Invalid input. Skipping ${fields[$i]}"
+				 echo "Invalid input. Skipping ${fields[$i]}" # still skipping
 			        ;;
 			esac
 		done		
 	elif [ $install2 == "y" ]; then
 		for i in "${!fields[@]}"; do
-			echo "Would you like to install ${fields[$i]}? (Y/n)"
+			echo "Would you like to install ${fields[$i]}? (Y/n)"# Literal copy of code above
 			read -r install3
 			case "$install3" in
 			 [Yy])
@@ -131,4 +129,4 @@ fi
 #else
 #	echo "aborting script."; exit
 #fi
-
+# Some old code here. It will be here for the reference
